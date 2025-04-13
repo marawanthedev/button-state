706470074f4a1fb10a6b2b9ed4c8c3f5bda41ee9
marawanthedev <tmarawan11@gmail.com>
2025-04-13 23:45:54 +0800

    style updates

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h1>🧠 React State vs Vanilla JS</h1>
 
-      <div style={{ display: "flex", gap: "40px", justifyContent: "center" }}>
-        <div>
+      <div style={{ display: "flex", gap: "40px", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <div style={{ width: "35%" }}>
           <h2>✅ React Way</h2>
           <ReactButtons />
         </div>
 
-        <div>
+        <div style={{ width: "35%" }}>
           <h2>❌ Vanilla JS Way</h2>
           <VanillaButtons />
         </div>
